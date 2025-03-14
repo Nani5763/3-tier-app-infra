013878fa2332c7682d5c1b876c6786e264aa18da
pavan <pavansaidevops457@gamil.com>
2025-03-14 10:14:55 +0530

    updates

--- PUBLIC-EC2-CREATE-AMI/backend-ec2.tf
+++ PUBLIC-EC2-CREATE-AMI/backend-ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "back" {
-    ami = ""
-    instance_type = ""
-    key_name = ""
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_name
     # subnet_id = aws_subnet.pub1.id
     # vpc_security_group_ids = [ aws_security_group.backend-server-sg.id ]
     user_data = templatefile("./backend.sh", {})
